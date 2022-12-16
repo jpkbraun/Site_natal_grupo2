2898015ed75fec7cd0d90822144a59ca28374e78
jpkbraun <jpk_braun@icloud.com>
2022-12-16 19:03:55 -0300

    menu#5

--- src/components/Produtos.js
+++ src/components/Produtos.js
@@ -10,16 +10,28 @@ import ProdutosLista3 from './ProdutosLista3';
 function Produtos() {
 
   let [opt, setOpt] = useState(<ProdutosLista1/>)
+  let [bt1, setBt1] = useState(<strong>Copa do Mundo</strong>)
+  let [bt2, setBt2] = useState('Natal')
+  let [bt3, setBt3] = useState('Ano Novo')
 
   return (
     <div className='cards' id='produtos'>
       <h1 className="background_produtos">PRODUTOS DIPONÍVEIS</h1>
       <h5 className="background_abas"><button className='button' 
-        onClick={()=>setOpt(<ProdutosLista1/>)}>Copa do Mundo</button></h5>
+        onClick={()=>{setOpt(<ProdutosLista1/>);
+        setBt1(<strong>Copa do Mundo</strong>);
+        setBt2('Natal');
+        setBt3('Ano Novo')}}>{bt1}</button></h5>
       <h5 className="background_abas"><button className='button' 
-        onClick={()=>setOpt(<ProdutosLista2/>)}>Natal</button></h5>
+        onClick={()=>{setOpt(<ProdutosLista2/>);
+        setBt1('Copa do Mundo');
+        setBt2(<strong>Natal</strong>);
+        setBt3('Ano Novo')}}>{bt2}</button></h5>
       <h5 className="background_abas"><button className='button' 
-        onClick={()=>setOpt(<ProdutosLista3/>)}>Ano Novo</button></h5>
+        onClick={()=>{setOpt(<ProdutosLista3/>);
+        setBt1('Copa do Mundo');
+        setBt2('Natal');
+        setBt3(<strong>Ano Novo</strong>)}}>{bt3}</button></h5>
       <div>
         <div>
         <span>{opt}</span>
